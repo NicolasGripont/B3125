1189b3d5161474918d7d03387ac3638c6b6f2ba9
Nicolas Gripont <Nicolas.Gripont@insa-lyon.fr>
2016-03-07 14:58:14 +0100

    création du projet DASI Gustatif

---
+++ DASI/Periode1/code/Gustatif/src/main/java/fr/insalyon/dasi/gustatif/vue/IHMConsole.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.dasi.gustatif.vue;
+
+/**
+ *
+ * @author Nico
+ */
+public class IHMConsole {
+    
+    public static void main(String[] args) {
+    // TODO code application logic here    public static void main(String[] args) {
+    // TODO code application logic here
+
+    } 
+
+}
