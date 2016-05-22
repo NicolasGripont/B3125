a7aa571ec96913cef3dbfd50a0fce0b7c60ae968
Nicolas Gripont <Nicolas.Gripont@insa-lyon.fr>
2016-05-22 19:00:06 +0200

    Fonctionne

--- DASI/Periode2/TP/src/main/java/fr/insalyon/dasi/collectif/ActionServlet.java
+++ DASI/Periode2/TP/src/main/java/fr/insalyon/dasi/collectif/ActionServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fr.insalyon.dasi.seance1;
+package fr.insalyon.dasi.collectif;
 
 import java.io.IOException;
 import java.io.PrintWriter;
